f84e891752702c0ee3650fd705e3560558f8f99c
Anna Kornieieva <109692085+KornieievaAnna@users.noreply.github.com>
2023-04-29 21:34:54 +0300

    add background-image

---
+++ src/pages/Home/Home.js
@@ -0,0 +1,14 @@
+import { Hero } from './Home.styled';
+
+export default function Home() {
+  return (
+    <Hero>
+      <h1>
+        Contact manager welcome page{' '}
+        <span role="img" aria-label="Greeting icon">
+          💁‍♀️
+        </span>
+      </h1>
+    </Hero>
+  );
+}
---
+++ src/pages/Home/Home.styled.js
@@ -0,0 +1,31 @@
+import styled from 'styled-components';
+
+
+export const Hero = styled.section`
+  padding-top: 200px;
+  padding-bottom: 200px;
+  background-image: linear-gradient(
+      rgba(47, 48, 58, 0.4),
+      rgba(47, 48, 58, 0.4)
+    ),
+    url(../images/background.jpg);
+  max-width: 1600px;
+  height: 600px;
+  margin: 0 auto;
+`;
+// const styles = {
+//   container: {
+//     paddingTop: '200px',
+//     paddingBottom: '200px',
+//     backgroundImage:
+//       'linear-gradient(rgba(47, 48, 58, 0.4), rgba(47, 48, 58, 0.4)),url(../images/background.jpg)',
+//     maxWidth: '1600px',
+//     height: '600px',
+//     margin: '0 auto',
+//   },
+//   title: {
+//     fontWeight: 500,
+//     fontSize: 48,
+//     textAlign: 'center',
+//   },
+// };
